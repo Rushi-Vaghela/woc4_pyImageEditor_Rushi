082e9d4e46e2916dd7b5ba9aabc44957d2df1b47
Rushi-Vaghela <91720066+Rushi-Vaghela@users.noreply.github.com>
2022-01-20 15:58:47 +0530

    Input Field

--- hello.py
+++ hello.py
@@ -1,12 +1,18 @@
 from tkinter import *
+from webbrowser import get
 
 root = Tk()
 
+e =Entry(root, width=50, bg="blue", fg="white", borderwidth=5)
+e.pack()
+e.insert(0, "Enter your Name: ")
+
 def myClick():
-    myLabel = Label(root, text="Look! I ckicked a button")
+    hello = "Hello " + e.get()
+    myLabel = Label(root, text = hello)
     myLabel.pack()
 
-myButton = Button(root, text="Click me!", command=myClick, fg="blue", bg="yellow")
+myButton = Button(root, text="Enter your Name", command=myClick, fg="blue", bg="yellow")
 myButton.pack()
 
 root.mainloop()
