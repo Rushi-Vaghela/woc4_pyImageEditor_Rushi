5259f8892084a6bd05fd4701ba6876f12d9730aa
Rushi-Vaghela <91720066+Rushi-Vaghela@users.noreply.github.com>
2022-01-20 15:48:38 +0530

    Button Tutotrial

--- hello.py
+++ hello.py
@@ -2,10 +2,11 @@ from tkinter import *
 
 root = Tk()
 
-myLabel1 = Label(root, text="Hello World!")
-myLabel2 = Label(root, text="My name is Rushi Vaghela")
+def myClick():
+    myLabel = Label(root, text="Look! I ckicked a button")
+    myLabel.pack()
 
-myLabel1.grid(row=0, column=0)
-myLabel2.grid(row=1, column=5)
+myButton = Button(root, text="Click me!", command=myClick, fg="blue", bg="yellow")
+myButton.pack()
 
 root.mainloop()
