c5fd40af0ed299e9cf2a9ae716fadb93c1445bb0
Rushi-Vaghela <91720066+Rushi-Vaghela@users.noreply.github.com>
2022-01-20 15:35:32 +0530

    Button Tutotrial

--- hello.py
+++ hello.py
@@ -2,8 +2,10 @@ from tkinter import *
 
 root = Tk()
 
-myLabel = Label(root, text="Hello World!")
+myLabel1 = Label(root, text="Hello World!")
+myLabel2 = Label(root, text="My name is Rushi Vaghela")
 
-myLabel.pack()
+myLabel1.grid(row=0, column=0)
+myLabel2.grid(row=1, column=5)
 
 root.mainloop()
