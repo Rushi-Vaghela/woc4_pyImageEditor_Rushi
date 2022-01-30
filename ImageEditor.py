9ba7edf6f171c50d1484c695641501fc22ce85a0
Rushi-Vaghela <91720066+Rushi-Vaghela@users.noreply.github.com>
2022-01-30 19:16:18 +0530

    Image Editor

--- ImageEditor.py
+++ ImageEditor.py
@@ -186,6 +186,96 @@ def sepia(canvas):
         canvas.data.undoQueue.append(canvas.data.image.copy())
         canvas.data.imageForTk=makeImageForTk(canvas)
         drawImage(canvas)
+
+def solarize(canvas):
+    canvas.data.colorPopToHappen=False
+    canvas.data.colorPopToHappen=False
+    solarizeWindow=Toplevel(canvas.data.mainWindow)
+    solarizeWindow.title("Solarize")
+    solarizeSlider=Scale(solarizeWindow, from_=0, to=255, orient=HORIZONTAL)
+    solarizeSlider.pack()
+    OkSolarizeFrame=Frame(solarizeWindow)
+    OkSolarizeButton=Button(OkSolarizeFrame, text="OK",\
+                            command=lambda: closeSolarizeWindow(canvas))
+    OkSolarizeButton.grid(row=0, column=0)
+    OkSolarizeFrame.pack(side=BOTTOM)
+    performSolarize(canvas, solarizeWindow, solarizeSlider, 255)
+
+def performSolarize(canvas, solarizeWindow, solarizeSlider, previousThreshold):
+    if canvas.data.solarizeWindowClose==True:
+        solarizeWindow.destroy()
+        canvas.data.solarizeWindowClose=False
+        
+    else:
+        # the  slider denotes the % of solarization thta the user wants,
+        # so the threshold (above which pixels are inverted) is inversely
+        # related to the slider value
+        if solarizeWindow.winfo_exists():
+            sliderVal=solarizeSlider.get()
+            threshold_=255-sliderVal
+            if canvas.data.image!=None and threshold_!=previousThreshold:
+                canvas.data.image=ImageOps.solarize(canvas.data.image,\
+                                                    threshold=threshold_)
+                canvas.data.imageForTk=makeImageForTk(canvas)
+                drawImage(canvas)
+            canvas.after(200, lambda: performSolarize(canvas, \
+                                solarizeWindow, solarizeSlider, threshold_))
+
+def closeSolarizeWindow(canvas):
+    if canvas.data.image!=None:
+        save(canvas)
+        canvas.data.undoQueue.append(canvas.data.image.copy())
+        canvas.data.solarizeWindowClose=True
+    
+def posterize(canvas):
+    canvas.data.colourPopToHappen=False
+    canvas.data.cropPopToHappen=False
+    canvas.data.drawOn=False
+    # we basically reduce the range of colurs from 256 to 5 bits
+    # and so, assign a single new value to each colour value
+    # in each succesive range
+    posterData=[]
+    if canvas.data.image!=None:
+        for col in range(canvas.data.imageSize[1]):
+            for row in range(canvas.data.imageSize[0]):
+                r, g, b= canvas.data.image.getpixel((row, col))
+                if r in range(32):
+                    R=0
+                elif r in range(32, 96):
+                    R=64
+                elif r in range(96, 160):
+                    R=128
+                elif r in range(160, 224):
+                    R=192
+                elif r in range(224,256):
+                    R=255
+                if g in range(32):
+                    G=0
+                elif g in range(32, 96):
+                    G=64
+                elif g in range(96, 160):
+                    G=128
+                elif r in range(160, 224):
+                    g=192
+                elif r in range(224,256):
+                    G=255
+                if b in range(32):
+                    B=0
+                elif b in range(32, 96):
+                    B=64
+                elif b in range(96, 160):
+                    B=128
+                elif b in range(160, 224):
+                    B=192
+                elif b in range(224,256):
+                    B=255
+                posterData.append((R, G, B))
+        canvas.data.image.putdata(posterData)
+        save(canvas)
+        canvas.data.undoQueue.append(canvas.data.image.copy())
+        canvas.data.imageForTk=makeImageForTk(canvas)
+        drawImage(canvas)
+
         
 def keyPressed(canvas, event):
     if event.keysym=="z":
@@ -340,7 +430,6 @@ def buttonsInit(root, canvas):
                        background=backgroundColour ,width=buttonWidth,\
                        height=buttonHeight, command=lambda: reset(canvas))
     resetButton.grid(row=9,column=0)
-    
     toolKitFrame.pack(side=RIGHT)
 
     
@@ -365,6 +454,9 @@ def menuInit(root, canvas):
 
     filtermenu.add_command(label="Sepia",\
                            command=lambda:sepia(canvas))
+
+    filtermenu.add_command(label="Solarize",\
+                           command=lambda:solarize(canvas))
     
     menubar.add_cascade(label="Filter", menu=filtermenu)
     root.config(menu=menubar)
